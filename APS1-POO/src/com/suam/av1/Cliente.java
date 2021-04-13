856c846e337df9e17f4bb49875a814caa3e078c7
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2021-04-13 22:54:23 +0000

    Remove APS1 e A1

--- APS1-POO/src/com/suam/av1/Cliente.java
+++
@@ -1,6 +0,0 @@
-package com.suam.av1;
-
-public class Cliente {
-	public String nome;
-	public int idade;
-}
--- APS1-POO/src/com/suam/av1/Empresa.java
+++
@@ -1,8 +0,0 @@
-package com.suam.av1;
-
-public class Empresa {
-	public String nome;
-	public String cnpj;
-	Funcionario funcionarios;
-	Cliente clientes;
-}
