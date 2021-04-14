59bf9c490c50c760023925b9b2669dd41c6623ef
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2021-04-14 20:52:32 +0000

    Add APS1 Teoria

---
+++ APS1-TeoriaComp/src/Principal.java
@@ -0,0 +1,37 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+	public static void main(String[]args) {
+		String string = JOptionPane.showInputDialog("Digite aqui seu alfabeto");
+		CharSequence ch;
+		
+		boolean value = false;
+		
+		while(!value) {	
+		ch = JOptionPane.showInputDialog("Digite uma palavra");
+		value = string.contains(ch);
+		
+		if(value == true) {
+			JOptionPane.showMessageDialog(null, montaExibicao(ch, string));
+		} 
+		if(value !=true ) {
+			JOptionPane.showMessageDialog(null, montaExibicaoErro(ch, string));
+		}
+	  }
+		System.exit(0);
+	}
+	public static String montaExibicao(CharSequence ch,String string) {
+		String exibicao = "";
+		exibicao+=" ===================\n";
+		exibicao+="A palavra "+ch+" pertence a palavra ∑ "+string;
+		exibicao+="\n ===================";
+		return exibicao;
+	}
+	public static String montaExibicaoErro(CharSequence ch,String string) {
+		String exibicao = "";
+		exibicao+=" ===================\n";
+		exibicao+="A palavra "+ch+ " não pertence a palavra ∑ "+string;
+		exibicao+="\n ===================";
+		return exibicao;
+	}
+}
